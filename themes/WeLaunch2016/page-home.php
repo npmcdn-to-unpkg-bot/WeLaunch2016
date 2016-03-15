c2508613d82fea9d58679b0bfb50bea4f5f926ba
Rob Hough <rob@welaunch.co.uk>
2016-03-15 12:23:23 +0000

    Update 15032016 RH

--- themes/WeLaunch2016/page-home.php
+++ themes/WeLaunch2016/page-home.php
@@ -58,21 +58,28 @@ get_header(); ?>
 <main id="wrapper" class="container">
 <!-- Portfolio Blocks -->
 <div id="portfolio" class="row">
-	<?php $the_query = new WP_Query( array('post_type' => 'casestudy', 'posts_per_page' => 12, 'order' => 'DESC') ); ?>
-	<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-		<a href="<?php the_permalink(); ?>">
-			<div class="folio-item" style="background-image: url('<?php $image_id = get_post_thumbnail_id();$image_url = wp_get_attachment_image_src($image_id,'large', true);echo $image_url[0];  ?>');">
-				<div class="folio-item-overlay">
-					<h4><?php the_title(); ?></h4>
-					<p>
-						<?php echo wp_trim_words( get_field('short_description'), 25, '...' ); ?>
-					</p>
-				</div>
+	<?php //$the_query = new WP_Query( array('post_type' => 'casestudy', 'posts_per_page' => 12, 'order' => 'DESC') ); ?>
+	<?php //while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+
+	<?php $posts = get_field('featured_projects'); ?>
+	<?php if( $posts ): ?>
+	<?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+		<?php setup_postdata($post); ?>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+				<a href="<?php the_permalink(); ?>">
+					<div class="folio-item" style="background-image: url('<?php $image_id = get_post_thumbnail_id();$image_url = wp_get_attachment_image_src($image_id,'large', true);echo $image_url[0];  ?>');">
+						<div class="folio-item-overlay">
+							<h4><?php the_title(); ?></h4>
+							<p>
+								<?php echo wp_trim_words( get_field('short_description'), 25, '...' ); ?>
+							</p>
+						</div>
+					</div>
+				</a>
 			</div>
-		</a>
-	</div>
-	<?php endwhile; ?>
+		<?php endforeach; ?>
+		<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+	<?php endif; ?>
 </div>
 <h3>Latest News</h3>
 <section id="home-latest">
--- themes/WeLaunch2016/page-work.php
+++ themes/WeLaunch2016/page-work.php
@@ -19,26 +19,57 @@ get_header(); ?>
 		<h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h1>
 	</div>
 </div>
-<section class="work-filter hidden-xs">
-	<ul class="work-filter-list" role="tablist" >
-		<li class="active"><a class="filter-btn btn btn-primary" data-filter="*">All</a></li>
-		<li><a class="filter-btn btn btn-primary" data-filter=".branding">Branding</a></li>
-		<li><a class="filter-btn btn btn-primary" data-filter=".digital">Digital</a></li>
-		<li><a class="filter-btn btn btn-primary" data-filter=".motion">Motion</a></li>
-		<li><a class="filter-btn btn btn-primary" data-filter=".print">Print</a></li>
-		<li><a class="filter-btn btn btn-primary" data-filter=".strategy">Strategy</a></li>
-		<li><a class="filter-btn btn btn-primary" data-filter=".events">Events</a></li>
+<section class="work-filter clearfix">
+	<ul class="work-filter-list">
+		<!-- <li><strong>Filter By:</strong></li> -->
+		<li class="active"><a class="filter-btn" data-filter="*">All</a></li>
+		<li><a title="Discipline" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" aria-expanded="true">Discipline <span class="caret"></span></a>
+			<ul role="menu" class="dropdown-menu discipline" role="tablist" >
+				<li><a class="filter-btn" data-filter=".Branding">Branding</a></li>
+				<li><a class="filter-btn" data-filter=".Digital">Digital</a></li>
+				<li><a class="filter-btn" data-filter=".Motion">Motion</a></li>
+				<li><a class="filter-btn" data-filter=".Print">Print</a></li>
+				<li><a class="filter-btn" data-filter=".Strategy">Strategy</a></li>
+				<li><a class="filter-btn" data-filter=".Strategy">Engagement</a></li>
+				<li><a class="filter-btn" data-filter=".Events">Destination</a></li>
+				<li><a class="filter-btn" data-filter=".Events">Internal Comms</a></li>
+			</ul>
+		</li>
+		<li>
+			<a title="Sector" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" aria-expanded="true">Sector <span class="caret"></span></a>
+			<ul role="menu" class="dropdown-menu discipline" role="tablist" >
+				<!-- List all Industry Sectors -->
+				<?php
+				$taxonomy = 'industry_sectors';
+				$tax_terms = get_terms($taxonomy);
+				?>
+				<?php
+				foreach ($tax_terms as $tax_term) {
+				echo '<li>' . '<a class="filter-btn" href="" data-filter=".'. $tax_term->slug. '" title="' . sprintf( __( "View all posts in %s" ), $tax_term->name ) . '" ' . '>' . $tax_term->name.'</a></li>';
+				}
+				?>
+			</ul>
+		</li>
+		<!-- <li>
+			<a title="Sector" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" aria-expanded="true">Yearly <span class="caret"></span></a>
+			<ul role="menu" class="dropdown-menu discipline" role="tablist" >
+				<li><a class="filter-btn" data-filter=".2016">2016</a></li>
+				<li><a class="filter-btn" data-filter=".2015">2015</a></li>
+				<li><a class="filter-btn" data-filter=".2014">2014</a></li>
+				<li><a class="filter-btn" data-filter=".2013">2013</a></li>
+			</ul>
+		</li> -->
 	</ul>
 </section>
 
 <!-- Portfolio Blocks -->
-<section>
+<section class="pad">
 <div id="portfolio" class="row filter-grid">
 	<?php $the_query = new WP_Query( array('post_type' => 'casestudy', 'posts_per_page' => -1, 'order' => 'DESC') ); ?>
 	<?php query_posts($query_string . '&cat=-163'); ?>
 	<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-		<?php $filter = Array('digital', 'branding', 'motion', 'print', 'strategy', 'events'); ?> <!-- Testing only -->
-	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 <?php echo $filter[array_rand($filter)]; ?>">
+	<?php $terms = get_the_terms($post->ID, "industry_sectors"); ?>
+	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 <?php the_field('discipline'); ?> <?php foreach ( $terms as $term ) { echo $term->slug . ' '; } ?> <?php echo get_the_time('Y'); ?>">
 		<a href="<?php the_permalink(); ?>">
 			<div class="folio-item" style="background-image: url('<?php $image_id = get_post_thumbnail_id();$image_url = wp_get_attachment_image_src($image_id,'large', true);echo $image_url[0];  ?>');">
 				<div class="folio-item-overlay">
