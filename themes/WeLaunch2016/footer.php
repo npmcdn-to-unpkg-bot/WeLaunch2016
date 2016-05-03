6b1806e0e230d97630637ddb1cce150cf5b75bc5
Rob Hough <rob@welaunch.co.uk>
2016-05-03 15:09:05 +0100

    Isotope Fix

--- themes/WeLaunch2016/footer.php
+++ themes/WeLaunch2016/footer.php
@@ -12,21 +12,7 @@
 ?>
 
 <?php get_template_part( 'template-parts/footer' ); ?>
-
 <?php wp_footer(); ?>
-<script src="http://f.vimeocdn.com/js/froogaloop2.min.js"></script>
-<script>
-   var iframe = $('#vimeo-player')[0],
-	   player = $f(iframe),
-	   status = $('.status');
 
-	   player.addEvent('ready', function() {
-		   player.api('setVolume', 0);
-		   player.api('autoplay', 1);
-	   });
-</script>
-<script>
-  new WOW().init();
-</script>
 </body>
 </html>
