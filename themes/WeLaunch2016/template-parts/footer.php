3dcc1826f20f16de33f03c419b120c0d4efc2358
Rob Hough <rob@welaunch.co.uk>
2016-05-03 10:17:45 +0100

    LinkedIn Update

--- themes/WeLaunch2016/template-parts/footer.php
+++ themes/WeLaunch2016/template-parts/footer.php
@@ -9,7 +9,7 @@
 	<div class="footer-social">
 		<a href="http://www.twitter.com/welaunch" target="_blank"><i class="fa fa-twitter"></i></a>
 		<a href="http://www.instagram.com/welaunch" target="_blank"><i class="fa fa-instagram"></i></a>
-		<a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+		<a href="https://www.linkedin.com/company/we-launch" target="_blank"><i class="fa fa-linkedin"></i></a>
 	</div>
 </footer>
 
